cd16a45278626c840f0d43646dd5858fc4cb5355
MUMUKSHMEGHWAL <mumukshmeghwal@gmail.com>
2022-01-28 02:36:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import homeImg from './home.png'
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Oval } from  'react-loader-spinner'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import { productList } from './product'
+
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = { 
+        searchedImage: "",
+        inputValue:"",
+        productList,
+        foundProducts: [],
+        showLoader: false
+    };
+  }
+
+  getProductFromImage = () => {
+    this.setState({
+      showLoader: true
+    })
+    const imgUrl = this.state.inputValue;
+    var myHeaders = new Headers();
+      myHeaders.append("accept", "application/x-www-form-urlencoded");
+      myHeaders.append("Authorization", "Basic cm1CcVpzT19scC1jZFdERVVPak5FTlZTODlpOHJaS0g6");
+      myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+      var urlencoded = new URLSearchParams();
+      urlencoded.append("urls", imgUrl);
+
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+      };
+
+      fetch("https://app.nanonets.com/api/v2/ObjectDetection/Model/4e8d57da-9f06-48fd-bd0a-3d798ab87ccc/LabelUrls/", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          if(result.result && result.result[0] && result.result[0].prediction) {
+            const products = Array.from(new Set(result.result[0].prediction.map(product => product.label && product.label.toLocaleLowerCase())))
+            this.setState({
+              foundProducts: products,
+              searchedImage: imgUrl,
+              showLoader: false
+            })
+
+          }
+        })
+        .catch(error => {
+          console.log('error', error)
+          this.setState({
+            showLoader: false
+          })
+        });
+  }
+
+  onSearchInputChange = (event) => {
+    if(event && event.target && event.target.value) {
+      this.setState({
+        inputValue: event.target.value
+      })
+    }
+  }
+ 
+  render () {
+    const {foundProducts, productList, searchedImage, showLoader}  = this.state
+    return (
+      <div className="App">
+        <header className="headerContainer">
+          {/* <img 
+            src="https://d3gq2merok8n5r.cloudfront.net/bumblebee/dummy_images/aaaa-1639389567-EcYX9/screenshot-2022-01-27-at-50833-pm-1643283597-HH6cs.png" 
+            width="100%"/> */}
+            <img src="https://d3gq2merok8n5r.cloudfront.net/bumblebee/dummy_images/aaaa-1639389567-EcYX9/screenshot-2022-01-27-at-115119-pm-1643307809-DW60e.png"
+              width="16%"
+              className
+            />
+            <div className="searchBarInputContainer">
+              <input className="searchBarInput"
+                onInput={this.onSearchInputChange}
+                placeholder="Search Products by Image"
+              />
+              <button  className="searchBtn" 
+                onClick={this.getProductFromImage}> Search</button>
+            </div>
+            <img src="https://d3gq2merok8n5r.cloudfront.net/bumblebee/dummy_images/aaaa-1639389567-EcYX9/screenshot-2022-01-27-at-115106-pm-1643307807-zdMs3.png"
+              width="36%"
+            />
+        </header>
+        {searchedImage && searchedImage.length ? <div>
+            <h3>Results based on the image</h3> 
+            <p className="productDetectedList">Detected Products: {foundProducts.toString()}</p>
+            <div className="cotainer">
+              <div className="imageContainer">
+                <img 
+                  src={searchedImage} 
+                  alt="product Image" 
+                  width="100%"
+                />
+              </div>
+              <div className="resultContainer">
+                {foundProducts.map(productType => {
+                  const products = productList[productType];
+                  return products && products.length ? productList[productType].map((product, i) => {     
+                    return (
+                    <a className="productContainer" href={product.productURL}>
+                    <div className="result">
+                      <div className="resultImageContainer">
+                        <img 
+                          src={product.imageLink}
+                          alt="product Image" 
+                          width="100%"
+                        />
+                      </div>
+                      <div className="productDescription">
+                        <p className="productPrice">${product.price}</p>
+                        <p className="productName">{product.productName}</p>
+                        <p className="productType">{productType}</p>
+                      </div>
+                      <div className="heartIcon" >
+
+                      <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="far" data-icon="heart" class="svg-inline--fa fa-heart fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"/></svg>
+                      </div>
+                    </div></a>) 
+                  })
+                  : null
+                })}
+              </div>
+            </div>
+        </div>: 
+        <div>
+          <img src={homeImg} width="100%"></img>
+        </div>}
+        {showLoader  && <div className="loaderContainer"><Oval
+          heigth="100"
+          width="100"
+          color='#eb595f'
+          ariaLabel='loading'
+        /></div>}
+      </div>
+    );
+}
 }
 
 export default App;
---
+++ src/product.js
@@ -0,0 +1,73 @@
+export const productList = {
+    "couch": [{
+        imageLink: "https://cdn.shopify.com/s/files/1/0491/8496/4771/products/Felicity_Blue_New_1100x.jpg?v=1637654661",
+        price: "25000",
+        productName: "Sofa cum bed",
+        productURL: "https://www.ukeyo.com/collections/sofas-and-sectional/products/felicity-sofa-cum-bed",
+        tags: ["blue","Soft"]
+    },{
+        productURL: "https://www.ukeyo.com/collections/sofas-and-sectional/products/barrington-3-seater-sofa-blue-sapphire-chenille",
+        price: "47,495",
+        productName: "Barrington 3 Seater Sofa",
+        imageLink: "https://cdn.shopify.com/s/files/1/0491/8496/4771/products/sofa-4-3-seater-fabric_1_66634ada-cde1-4b79-8350-e348f09eb6c4_1100x.jpg?v=1625744269",
+        tags: ["blue","Soft"]
+    },{
+        productURL: "https://www.ukeyo.com/collections/sofas-and-sectional/products/winchester-3-seater-sofa",
+        price: "53914",
+        productName: "Winchester 3 Seater Sofa",
+        imageLink: "https://cdn.shopify.com/s/files/1/0491/8496/4771/products/winchester-3-seater-sofa_1_1100x.jpg?v=1611753587",
+        tags: ["blue","Soft"]
+    }]
+,
+    "chair": [{
+        productURL: "https://www.ukeyo.com/collections/all-dining-tables/products/urban-ladder-liana-zella-4-seater-dining-table-set",
+        price: "25000",
+        productName: "Zella 4 Seater Dining Table Set",
+        imageLink: "https://cdn.shopify.com/s/files/1/0491/8496/4771/products/Screenshot2022-01-27111829_500x.png?v=1643263417",
+        tags: ["Teak","WooD"]
+    },{
+        productURL: "https://www.ukeyo.com/collections/all-dining-tables/products/catria-4-seater-dining-table-teak",
+        price: "47,495",
+        productName: "Catria Teak 4 Seater Dining Table",
+        imageLink: "https://cdn.shopify.com/s/files/1/0491/8496/4771/products/1_0ca302de-8cd0-46fa-b2aa-54868a29c0b3_1100x.jpg?v=1614311709",
+        tags:  ["Teak","WooD"]
+    }
+    ]
+,
+    "stool": [{
+        productURL: "https://www.ukeyo.com/products/browny-storage-stool?_pos=16&_sid=6f22caa67&_ss=r",
+        price: "5000",
+        productName: "Browny Storage Stool",
+        imageLink: "https://cdn.shopify.com/s/files/1/0491/8496/4771/products/DSC_0015_8bad9ffc-31f7-45ab-a858-125eddb6f9f2_1100x.jpg?v=1631165647",
+        tags: ["Teak","WooD"]
+    },{
+        productURL: "https://www.ukeyo.com/products/adroit-furnish-natural-round-industrial-coffee-table?_pos=53&_sid=1fb16989c&_ss=r",
+        price: "7,495",
+        imageLink: "https://cdn.shopify.com/s/files/1/0491/8496/4771/products/fr1417_02_1100x.png?v=1641205667",
+        tags:  ["Teak","WooD"]
+    },{
+        productURL: "https://www.ukeyo.com/products/wooden-coffee-table?_pos=12&_sid=8f60d1c9a&_ss=r",
+        price: "17,495",
+        imageLink: "https://cdn.shopify.com/s/files/1/0491/8496/4771/products/DSC_0176_ee14f288-3528-4196-910f-191a36c929c8_1100x.jpg?v=1631164915",
+        tags:  ["Teak","WooD"]
+    }
+    ],
+    "drawer": [{
+        productURL: "https://www.ukeyo.com/products/metric-chest-of-drawers-of-drawer?_pos=1&_sid=7c49600d0&_ss=r",
+        price: "35000",
+        productName: "Orange Tree Drawer",
+        imageLink: "https://cdn.shopify.com/s/files/1/0491/8496/4771/products/Gallery_2_Metric-Chest-Of-Drawer-wooden-chest-of-drawers-online-india_1100x.jpg?v=1625476285",
+        tags: ["Teak","WooD"]
+    },{
+        productURL: "https://www.ukeyo.com/products/dewdrop-chest-of-drawers?_pos=8&_sid=7c49600d0&_ss=r",
+        price: "27,495",
+        productName: "Wooden Tree Drawer",
+        imageLink: "https://cdn.shopify.com/s/files/1/0491/8496/4771/products/dewdrop-chest-of-drawers_1_1100x.jpg?v=1606287765",
+        tags:  ["Teak","WooD"]
+    }
+    ]
+}
+
+
+
+
